c5590446f515beb0c521b0f76657c57aa69d6661
Gabriel Fairbairn <gabrielfair@hotmail.com>
2023-10-11 01:56:30 -0700

    lesson code: react, TS interface, destructure

--- apps/app/src/pages/new/opportunity.tsx
+++ apps/app/src/pages/new/opportunity.tsx
@@ -1,5 +1,34 @@
+import Link from 'next/link'
+
+interface testNames {
+  something1: string
+  something: string
+}
+
+const people: testNames = {
+  something1: 'Gabe',
+  something: 'Ryan'
+}
+
 const Opportunity = () => {
-  return <>Opportunities by Gabe</>
+  return (
+    <>
+      <Link href="http://localhost:3000/p/volunteer/nerdslayer0.test">
+        Go to Profile
+      </Link>
+      <Vhc something1={people.something1} something={people.something} />
+    </>
+  )
+}
+
+export const Vhc = (testNames: testNames) => {
+  return (
+    <>
+      {testNames.something1}
+
+      {testNames.something}
+    </>
+  )
 }
 
 export default Opportunity
